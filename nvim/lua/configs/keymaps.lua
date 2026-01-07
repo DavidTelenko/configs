1b981c103b1b76c5f75df167f598da8574cc3ced
David Telenko <davidtelenko11@gmail.com>
2026-01-07 03:07:12 +0200

    refactor(nvim): organize keymaps

--- nvim/lua/configs/keymaps.lua
+++ nvim/lua/configs/keymaps.lua
@@ -1,205 +1,168 @@
--- Keymaps for better default experience
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', {
-  silent = true,
-})
+local silent_expr = { expr = true, silent = true }
+local silent = { silent = true }
+local map = vim.keymap.set
 
-vim.keymap.set({ 'n', 'i', 'v' }, '<F1>', '<Esc>', {
-  silent = true,
-})
+do -- Un-maps (some clumsiness / muscle memory unlearning)
+  map({ 'n', 'v' }, '<Space>', '<NOP>', silent)
+  map({ 'n', 'i', 'v' }, '<F1>', '<NOP>', silent)
 
--- Remaps system-wide remapped keys
-vim.keymap.set({ 'n', 'v' }, '<A-u>', '<C-u>zz')
-vim.keymap.set({ 'n', 'v' }, '<PageUp>', '<C-u>zz')
+  map('n', '<C-u>', '<NOP>')
+  map('n', '<C-d>', '<NOP>')
+  map('n', '<C-o>', '<NOP>')
+  map('n', '<C-i>', '<NOP>')
 
-vim.keymap.set({ 'n', 'v' }, '<A-d>', '<C-d>zz')
-vim.keymap.set({ 'n', 'v' }, '<PageDown>', '<C-d>zz')
-
-vim.keymap.set({ 'n', 'v' }, '<A-o>', '<C-o>')
-vim.keymap.set({ 'n', 'v' }, '<A-i>', '<C-i>')
-vim.keymap.set({ 'n', 'v' }, '<A-r>', '<C-r>')
-
--- L and H - begin and end of the line
-vim.keymap.set({ 'n', 'v' }, 'L', '$')
-vim.keymap.set({ 'n', 'v' }, 'H', '_')
-
--- Jump to begin end of a tag
-vim.keymap.set('n', ']t', 'vat<esc>', { desc = 'Next tag' })
-vim.keymap.set('n', '[t', 'vato<esc>', { desc = 'Prev tag' })
-
--- Paste without reyanking
--- vim.keymap.set('v', 'p', '"0p')
-vim.keymap.set('v', 'p', 'P', {
-  desc = 'Paste without copying selected text in visual mode',
-})
-
--- Unlearning muscle memory
-vim.keymap.set('n', '<C-u>', '<NOP>')
-vim.keymap.set('n', '<C-d>', '<NOP>')
-vim.keymap.set('n', '<C-o>', '<NOP>')
-vim.keymap.set('n', '<C-i>', '<NOP>')
-
--- Disable default control keymaps
-vim.keymap.set('i', '<C-k>', '<NOP>', {})
-vim.keymap.set('i', '<C-j>', '<NOP>', {})
-
--- Remaps for dealing with word wrap
-vim.keymap.set('n', '0', "v:count == 0 ? 'g0' : '0'", {
-  expr = true,
-  silent = true,
-})
-
-vim.keymap.set('n', '$', "v:count == 0 ? 'g$' : '$'", {
-  expr = true,
-  silent = true,
-})
-
-vim.keymap.set({ 'n', 'v' }, 'k', "v:count == 0 ? 'gk' : 'k'", {
-  expr = true,
-  silent = true,
-})
-
-vim.keymap.set({ 'n', 'v' }, 'j', "v:count == 0 ? 'gj' : 'j'", {
-  expr = true,
-  silent = true,
-})
-
--- Remap asymmetric + -
-vim.keymap.set('n', '_', '-')
-
--- Navigation in insert mode
-vim.keymap.set('i', '<A-k>', '<up>', {
-  desc = 'Navigation up in insert mode',
-})
-
-vim.keymap.set('i', '<A-h>', '<left>', {
-  desc = 'Navigation left in insert mode',
-})
-
-vim.keymap.set('i', '<A-l>', '<right>', {
-  desc = 'Navigation right in insert mode',
-})
-
-vim.keymap.set('i', '<A-j>', '<down>', {
-  desc = 'Navigation down in insert mode',
-})
-
-vim.keymap.set('i', '<A-b>', '<C-o>b', {
-  desc = 'Navigation back word in insert mode',
-})
-
-vim.keymap.set('i', '<A-w>', '<C-o>w', {
-  desc = 'Navigation forward word in insert mode',
-})
-
-vim.keymap.set({ 'i', 't' }, '<C-H>', '<C-w>', {
-  desc = 'Delete word backward',
-})
-
-vim.keymap.set({ 'i', 't' }, '<A-BS>', '<C-w>', {
-  desc = 'Delete word backward',
-})
-
-vim.keymap.set({ 'i', 't' }, '<C-D>', '<C-o>dw', {
-  desc = 'Delte word forward',
-})
-
--- How do i exit terminal in vim?
-vim.keymap.set('t', '<esc>', '<C-\\><C-n>', {
-  desc = 'Exit terminal',
-})
-
--- Lua execute
-vim.keymap.set('n', '<leader>cX', '<cmd>source %<CR>', {
-  desc = 'Execute this file with lua',
-})
-
-vim.keymap.set('n', '<leader>cx', '<cmd>.lua<CR>', {
-  desc = 'Execute current line with lua',
-})
-
-vim.keymap.set('v', '<leader>cx', '<cmd>lua<CR>', {
-  desc = 'Execute current selection with lua',
-})
-
--- One more % to not use symbol row
-vim.keymap.set('n', 'gm', '%', { desc = 'Jump to closing' })
-
--- More convenient window navigation
-vim.keymap.set('n', 'gh', '<C-w>h', { desc = 'Jump to left window' })
-vim.keymap.set('n', 'gj', '<C-w>j', { desc = 'Jump to bottom window' })
-vim.keymap.set('n', 'gk', '<C-w>k', { desc = 'Jump to top window' })
-vim.keymap.set('n', 'gl', '<C-w>l', { desc = 'Jump to right window' })
-
--- Command sugar for replace
-vim.keymap.set('v', '<leader>rr', '"hy:%s/<C-r>h//g<left><left>', {
-  desc = 'Replace current selection',
-})
-
-vim.keymap.set('n', '<leader>rr', '"hyiw:%s/<C-r>h//g<left><left>', {
-  desc = 'Replace word under cursor',
-})
-
--- Buffer related keymaps
-vim.keymap.set('n', '<leader><cr>', function()
-  vim.cmd 'only'
-end, { desc = 'Focus current buffer' })
-
-vim.keymap.set('n', '<leader>fx', function()
-  vim.cmd '%bd'
-end, { desc = 'Delete all buffers' })
-
--- File / filepath related keymaps
-vim.keymap.set('n', '<leader>fy', function()
-  vim.fn.setreg('*', vim.fn.expand '%:.')
-end, { desc = 'Yank path' })
-
-vim.keymap.set('n', '<leader>fh', function()
-  vim.cmd 'TOhtml'
-end, { desc = 'To HTML' })
-
-vim.keymap.set('n', '<leader>fs', ':w !sudo tee %<cr>', {
-  desc = 'Sudo save',
-})
-
-vim.keymap.set('n', '<leader>f\\/', function()
-  vim.cmd '%s/\\\\/\\//g'
-end, { desc = 'Replace \\ with /' })
-
--- Zen mode
-vim.keymap.set('n', '<leader>z', require('helpers.zen').toggle_zen_mode, {
-  desc = 'Zen Mode',
-})
-
--- Toggle quickfix
-vim.keymap.set('n', '<leader>q', function()
-  vim.cmd 'ToggleQuickfix'
-end, {
-  silent = true,
-  desc = 'Toggle quickfix',
-})
-
--- Tabs
-for i = 1, 9 do
-  vim.keymap.set({ 'n', 'i', 't' }, '<M-' .. i .. '>', function()
-    pcall(vim.cmd, 'tabn' .. i)
-  end)
-  vim.keymap.set({ 'n' }, 'g' .. i, function()
-    pcall(vim.cmd, 'tabn' .. i)
-  end)
+  map('i', '<C-k>', '<NOP>')
+  map('i', '<C-j>', '<NOP>')
 end
 
-vim.keymap.set({ 'n' }, 'gt', function()
-  vim.cmd 'tabnew'
-end)
+do -- System-wide remapped keys
+  map({ 'n', 'v' }, '<A-u>', '<C-u>zz')
+  map({ 'n', 'v' }, '<PageUp>', '<C-u>zz')
 
-vim.keymap.set({ 'n', 'i', 't' }, '<C-S-t>', function()
-  vim.cmd 'tabnew'
-end)
+  map({ 'n', 'v' }, '<A-d>', '<C-d>zz')
+  map({ 'n', 'v' }, '<PageDown>', '<C-d>zz')
 
-vim.keymap.set({ 'n', 'i', 't' }, '<C-S-w>', function()
-  vim.cmd 'q'
-end)
+  map({ 'n', 'v' }, '<A-o>', '<C-o>')
+  map({ 'n', 'v' }, '<A-i>', '<C-i>')
+  map({ 'n', 'v' }, '<A-r>', '<C-r>')
+end
 
-vim.keymap.set({ 'n', 'i' }, '<A-s>', function()
-  vim.cmd 'noa update'
-end)
+do -- Jumps
+  -- L and H - begin and end of the line
+  -- Who uses Low Middle High after all?
+  map({ 'n', 'v' }, 'L', '$')
+  map({ 'n', 'v' }, 'H', '_')
+
+  -- Begin end of a tag
+  map('n', ']t', 'vat<esc>', { desc = 'Next tag' })
+  map('n', '[t', 'vato<esc>', { desc = 'Prev tag' })
+end
+
+do -- Go to
+  -- Matching bracket
+  map('n', 'gm', '%', { desc = 'Matching' })
+
+  -- Windows (panes)
+  map('n', 'gh', '<C-w>h', { desc = 'Left pane' })
+  map('n', 'gj', '<C-w>j', { desc = 'Bottom pane' })
+  map('n', 'gk', '<C-w>k', { desc = 'Top pane' })
+  map('n', 'gl', '<C-w>l', { desc = 'Right pane' })
+end
+
+do -- QoL improvements
+  -- Remap asymmetric + -
+  map('n', '_', '-')
+
+  -- Paste without reyanking
+  map('v', 'p', 'P', {
+    desc = 'Paste without copying selected text in visual mode',
+  })
+
+  -- Navigation in insert mode
+  map('i', '<A-k>', '<up>')
+  map('i', '<A-h>', '<left>')
+  map('i', '<A-l>', '<right>')
+  map('i', '<A-j>', '<down>')
+  map('i', '<A-b>', '<C-o>b')
+  map('i', '<A-w>', '<C-o>w')
+
+  -- Actions in insert mode
+  map({ 'i', 't' }, '<C-H>', '<C-w>')
+  map({ 'i', 't' }, '<A-BS>', '<C-w>')
+  map({ 'i', 't' }, '<C-D>', '<C-o>dw')
+
+  -- Remaps for dealing with word wrap
+  map('n', '0', "v:count == 0 ? 'g0' : '0'", silent_expr)
+  map('n', '$', "v:count == 0 ? 'g$' : '$'", silent_expr)
+  map({ 'n', 'v' }, 'k', "v:count == 0 ? 'gk' : 'k'", silent_expr)
+  map({ 'n', 'v' }, 'j', "v:count == 0 ? 'gj' : 'j'", silent_expr)
+
+  -- How do i exit terminal in vim?
+  map('t', '<esc>', '<C-\\><C-n>')
+
+  -- Toggle quickfix
+  map('n', '<leader>q', function()
+    vim.cmd 'ToggleQuickfix'
+  end, {
+    silent = true,
+    desc = 'Toggle quickfix',
+  })
+
+  -- Zen mode
+  map('n', '<leader>z', require('helpers.zen').toggle_zen_mode, {
+    desc = 'Zen Mode',
+  })
+
+  -- Command sugar to replace current selection
+  local desc = { desc = 'Replace current selection' }
+  map('v', '<leader>rr', '"hy:%s/<C-r>h//g<left><left>', desc)
+  map('n', '<leader>rr', '"hyiw:%s/<C-r>h//g<left><left>', desc)
+end
+
+do -- Buffer related keymaps
+  map('n', '<leader><cr>', function()
+    vim.cmd 'only'
+  end, { desc = 'Focus current buffer' })
+
+  map('n', '<leader>fx', function()
+    vim.cmd '%bd'
+  end, { desc = 'Delete all buffers' })
+end
+
+do -- File / Path related keymaps
+  map('n', '<leader>fy', function()
+    vim.fn.setreg('*', vim.fn.expand '%:.')
+  end, { desc = 'Yank path' })
+
+  map('n', '<leader>fh', function()
+    vim.cmd 'TOhtml'
+  end, { desc = 'To HTML' })
+
+  map('n', '<leader>fs', ':w !sudo tee %<cr>', {
+    desc = 'Sudo save',
+  })
+
+  map('n', '<leader>f\\/', function()
+    vim.cmd '%s/\\\\/\\//g'
+  end, { desc = 'Replace \\ with /' })
+end
+
+do -- Tabs
+  map({ 'n' }, 'gt', function()
+    vim.api.nvim_exec2('tabnew', {})
+  end)
+
+  for i = 1, 9 do
+    map({ 'n', 'i', 't' }, '<M-' .. i .. '>', function()
+      pcall(vim.api.nvim_exec2, 'tabn' .. i, {})
+    end)
+
+    map({ 'n' }, 'g' .. i, function()
+      pcall(vim.api.nvim_exec2, 'tabn' .. i, {})
+    end)
+  end
+end
+
+do -- Lua execute
+  map('n', '<leader>cX', '<cmd>source %<CR>', {
+    desc = 'Execute this file with lua',
+  })
+
+  map('n', '<leader>cx', '<cmd>.lua<CR>', {
+    desc = 'Execute current line with lua',
+  })
+
+  map('v', '<leader>cx', '<cmd>lua<CR>', {
+    desc = 'Execute current selection with lua',
+  })
+end
+
+do -- to Common User Access or not to Common User Access?
+  map({ 'n', 'i', 't' }, '<C-S-w>', function()
+    vim.cmd 'q'
+  end)
+
+  map({ 'n', 'i' }, '<A-s>', function()
+    vim.cmd 'noa update'
+  end)
+end
--- nvim/lua/helpers/lsp.lua
+++ nvim/lua/helpers/lsp.lua
@@ -8,7 +8,7 @@ M.on_attach = function(client, buffer)
   local map = function(keys, desc, func)
     vim.keymap.set('n', keys, func, {
       buffer = buffer,
-      desc = desc .. ' [lsp]',
+      desc = desc,
     })
   end
 
@@ -31,54 +31,71 @@ M.on_attach = function(client, buffer)
     end
   end
 
-  map_next_prev_diagnostic {
-    severity = vim.diagnostic.severity.ERROR,
-    key = 'e',
-    message = 'error',
-  }
-  map_next_prev_diagnostic {
-    severity = vim.diagnostic.severity.WARN,
-    key = 'w',
-    message = 'warning',
-  }
-  map_next_prev_diagnostic {
-    severity = nil,
-    key = 'd',
-    message = 'diagnostic',
-  }
-
-  map('<leader>rn', 'Rename', vim.lsp.buf.rename)
-  map('<leader>rf', 'Format', vim.lsp.buf.format)
-  map('<leader>ca', 'Code Action', vim.lsp.buf.code_action)
-  map('<leader>cI', 'Toggle inlay hints', function()
-    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
-  end)
-
-  map('gd', 'Jump to definition', telescope.lsp_definitions)
-  map('gR', 'Jump to references', telescope.lsp_references)
-  map('gI', 'Jump to implementation', telescope.lsp_implementations)
-  if client:supports_method('textDocument/declaration', buffer) then
-    map('gD', 'Jump to declaration', vim.lsp.buf.declaration)
+  do -- move
+    map_next_prev_diagnostic {
+      severity = vim.diagnostic.severity.ERROR,
+      key = 'e',
+      message = 'error',
+    }
+    map_next_prev_diagnostic {
+      severity = vim.diagnostic.severity.WARN,
+      key = 'w',
+      message = 'warning',
+    }
+    map_next_prev_diagnostic {
+      severity = nil,
+      key = 'd',
+      message = 'diagnostic',
+    }
   end
 
-  map('<leader>ss', 'Document Symbols', telescope.lsp_document_symbols)
+  do -- goto
+    -- TODO: experimenting with this
+    -- map('gd', 'Definition', telescope.lsp_definitions)
+    -- map('gR', 'References', telescope.lsp_references)
+    -- map('gI', 'Implementation', telescope.lsp_implementations)
 
-  map('K', 'Hover Documentation', function()
-    vim.lsp.buf.hover { border = 'rounded' }
-  end)
+    map('gd', 'Definition', vim.lsp.buf.definition)
+    map('gR', 'References', vim.lsp.buf.references)
+    map('gI', 'Implementation', vim.lsp.buf.implementation)
 
-  -- Lesser used LSP functionality
-  map('<leader>wa', 'Add Folder', vim.lsp.buf.add_workspace_folder)
-  map('<leader>ws', 'Symbols', telescope.lsp_dynamic_workspace_symbols)
-  map('<leader>wr', 'Remove Folder', vim.lsp.buf.remove_workspace_folder)
-  map('<leader>wl', 'List Folders', function()
-    vim.print(vim.lsp.buf.list_workspace_folders())
-  end)
+    if client:supports_method('textDocument/declaration', buffer) then
+      map('gD', 'Declaration', vim.lsp.buf.declaration)
+    end
+  end
 
-  -- Never used it???
-  -- map('gK', 'Signature Documentation', function()
-  --   vim.lsp.buf.signature_help { border = 'rounded' }
-  -- end)
+  do -- actions
+    map('<leader>rn', 'Rename', vim.lsp.buf.rename)
+    map('<leader>rf', 'Format', vim.lsp.buf.format)
+    map('<leader>ca', 'Code Action', vim.lsp.buf.code_action)
+    map('<leader>cI', 'Toggle inlay hints', function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
+    end)
+  end
+
+  do -- search
+    map('<leader>ss', 'Document Symbols', telescope.lsp_document_symbols)
+  end
+
+  do --help
+    map('K', 'Hover Documentation', function()
+      vim.lsp.buf.hover { border = 'rounded' }
+    end)
+
+    -- Never used it???
+    -- map('gK', 'Signature Documentation', function()
+    --   vim.lsp.buf.signature_help { border = 'rounded' }
+    -- end)
+  end
+
+  do -- workspace
+    map('<leader>wa', 'Add Folder', vim.lsp.buf.add_workspace_folder)
+    map('<leader>ws', 'Symbols', telescope.lsp_dynamic_workspace_symbols)
+    map('<leader>wr', 'Remove Folder', vim.lsp.buf.remove_workspace_folder)
+    map('<leader>wl', 'List Folders', function()
+      vim.print(vim.lsp.buf.list_workspace_folders())
+    end)
+  end
 end
 
 return M

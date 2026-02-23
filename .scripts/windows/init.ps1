4c2dfcc34035a3d8e682b777fbd025c3525a9cd1
David Telenko <davidtelenko11@gmail.com>
2026-02-23 03:07:34 +0200

    feat: disable spicetify autoinstall for now

--- .scripts/windows/init.ps1
+++ .scripts/windows/init.ps1
@@ -153,6 +153,6 @@ if ($Git)
 if ($Spotify)
 {
   Invoke-Expression "& { $(Invoke-WebRequest -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify"
-  Invoke-WebRequest -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | Invoke-Expression
-  Invoke-WebRequest -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | Invoke-Expression
+  # Invoke-WebRequest -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | Invoke-Expression
+  # Invoke-WebRequest -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | Invoke-Expression
 }

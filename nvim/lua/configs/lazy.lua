231cd5a77870d7432f96e509fce52cbadab35619
David Telenko <davidtelenko11@gmail.com>
2026-04-22 01:13:34 +0300

    chore(nvim): remove matchparen plugin
    - Cannot clearly distinguish between current cursor and matching cursor

--- nvim/lua/configs/lazy.lua
+++ nvim/lua/configs/lazy.lua
@@ -98,7 +98,7 @@ require('lazy').setup {
       disabled_plugins = {
         'gzip',
         'matchit',
-        -- 'matchparen',
+        'matchparen',
         'netrwPlugin',
         'tarPlugin',
         -- 'tohtml',
